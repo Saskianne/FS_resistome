79169ecc6f3a158554cf9a0d085716ec02894f93
Saskianne <157356003+Saskianne@users.noreply.github.com>
2025-02-20 17:51:09 +0100

    2025.02.20_end

--- Assembly/2_5_Assembly_QCs.sh
+++ Assembly/2_5_Assembly_QCs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH -c 32                      # 1 core per job (i.e., if you need 8 cores, you would have to use "-c 8")
+#SBATCH -c 32                       # 1 core per job (i.e., if you need 8 cores, you would have to use "-c 8")
 #SBATCH --job-name=QC_total
-#SBATCH -t 10-00:00                # Runtime in D-HH:MM
+#SBATCH -t 10-00:00                 # Runtime in D-HH:MM
 #SBATCH --qos=long                  # quality of service parameters
-#SBATCH -p base                  # Partition to submit to
-#SBATCH --mem=150G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -p base                     # Partition to submit to
+#SBATCH --mem=150G                  # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=Illumina_assem_QC.out
 #SBATCH --error=Illumina_assem_QC.err
 
 # here starts your actual program call/computation
 #
+
 cd /gxfs_work/geomar/smomw681/DATA/QC_ALL
 module load gcc12-env/12.3.0
 module load miniconda3/24.11.1
@@ -19,18 +20,43 @@ echo "START TIME": '' $(date)
 
 # Set up variables
 
-dir1="/gxfs_work/geomar/smomw681/DATA/RAWDATA/QC_ALL"
+dir1="/gxfs_work/geomar/smomw681/DATA/QC_RESULTS"
 dir2="/gxfs_work/geomar/smomw681/DATA/RAWDATA"
-dir3="/gxfs_work/geomar/smomw681/DATA/QCed_DATA"
-dir4="/gxfs_work/geomar/smomw681/DATA/PHIX_FILTERED"
-dir5="/gxfs_work/geomar/smomw681/DATA/ERROR_CORRECTED"
-dir6="/gxfs_work/geomar/smomw681/DATA/ASSEMBLIES"
+dir3="$dir1/RAW_fastqc"
+
+dir4="/gxfs_work/geomar/smomw681/DATA/QCed_DATA"
+dir5="$dir1/TRIM_fastqc"
+
+FILES=($dir2/*_1.fastq.gz)
+BASES=(basename ${FILES[@]} "_1.fastq.gz")
 
 # iterate over fastq files
-for fastq_file in ${dir2}/*.fastq.gz; do
-     fastqc -t 4 -f fastq -noextract $fastq_file -o ${dir1};
-     multiqc -o ${dir1} ${dir1}
-done
+srun --pty --x11 --mem=10G -N 1 -p interactive -t 12:00:00 \
+    bash -c " \
+    fastqc -f fastq -t 4 * .fastq -noextract -o $dir3 ${FILES[@]} ; \
+    multiqc -o $dir3/*_fastqc.zip -n RAW_fastqc_summary.txt ${dir3} "  \
+    bash -c " \
+    fastqc -f fastq -t 4 * .fastq -noextract -o $dir5/${BASES} "${BASES[@]}.fg"; \
+    multiqc -o $dir1 ${dir3} "  \    
+    
+    
+
+
+
+
+# dir6="/gxfs_work/geomar/smomw681/DATA/PHIX_FILTERED"
+# dir7="$dir1/FILTER_fastqc"
+
+# dir8="/gxfs_work/geomar/smomw681/DATA/ERROR_CORRECTED"
+# dir9="$dir1/CORRECTION_fastqc"
+
+
+# dir10="/gxfs_work/geomar/smomw681/DATA/ASSEMBLIES"
+# dir11="$dir1/ASSEMBLY_fastqc"
+
+
+exit
+
 
 
 echo "END TIME": '' $(date)
--- link_tree.md
+++ link_tree.md
@@ -10,13 +10,18 @@ Bioinformatic tools:
     - parallel fastq dump (bioconda::parallel-fastq-dump, v0.6.7)
     - parallel (conda-forge::parallel, v20241222)
 - Assembly
+    - fastqc(v0.12.1)
+        - https://hbctraining.github.io/Intro-to-rnaseq-hpc-salmon-flipped/lessons/05_qc_running_fastqc_interactively.html
+        - examples: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+    - multiqc(v1.27.1)
+        - https://docs.seqera.io/multiqc/getting_started/running_multiqc
     - metaSPAdes (bioconda::spades, v4.0.0)
     - Trimmomatric (bioconda::trimmomatic, v0.39)
     - bbmap (bioconda::bbmap, v39.15)
     - wtdbg2(v2.5)
         - https://github.com/ruanjue/wtdbg2
     - Unicycler (v0.5.1)
-        - 
+        - https://github.com/rrwick/Unicycler?tab=readme-ov-file#method-long-read-only-assembly
     - (meta)Flye (v2.9.5)
         - https://github.com/mikolmogorov/Flye/blob/flye/docs/USAGE.md
     - Canu (v2.3)
@@ -37,4 +42,6 @@ Bioinformatic tools:
 General understanding:
 - Metagenome assembly with metaSPAdes: https://carpentries-lab.github.io/metagenomics-analysis/04-assembly/index.html
 - conda: https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-pkgs.html
+- general bash & shell: https://www.gnu.org/software/bash/manual/bash.html#Installing-Bash
+
 
