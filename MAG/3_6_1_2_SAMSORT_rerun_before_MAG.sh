61f4f2447293a3cca8a0fd4b9ff83d9186a5f609
Saskianne <157356003+Saskianne@users.noreply.github.com>
2025-03-10 18:56:17 +0100

    2025.03.10_end

--- MAG/3_6_1_2_SAMSORT_rerun_before_MAG.sh
+++ MAG/3_6_1_2_SAMSORT_rerun_before_MAG.sh
@@ -5,15 +5,14 @@
 #SBATCH --qos=long                  # quality of service parameters
 #SBATCH -p base                  # Partition to submit to                  # Partition to submit to
 #SBATCH --mem=50G                 # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=JGI_Cov2.out
-#SBATCH --error=JGI_Cov2.err
+#SBATCH --output=JGI_Co3.out
+#SBATCH --error=JGI_Cov3.err
 # here starts your actual program call/computation
 #
 echo "START TIME": '' $(date)
 
 module load gcc12-env/12.3.0
 module load miniconda3/24.11.1
-conda activate Assembly
 
 cd /gxfs_work/geomar/smomw681/DATA/MAG_Illumina/METABAT2/BAMFILEs
 
@@ -26,30 +25,30 @@ COV_Files="/gxfs_work/geomar/smomw681/DATA/MAG_Illumina/METABAT2/COVERAGE_FILEs"
 
 echo starting with samtools sort at $(date)
 
-for sample in ${BAM_FILEs}/*.out.bam;
-do
-base=$(basename $sample ".out.bam")
-if [ ! -f ${RERUN_SAMSORT}/${base}.out.sorted.bam ]; then
-    echo working on $base
-    samtools sort \
-        -o ${RERUN_SAMSORT}/${base}.out.sorted.bam \
-        --output-fmt BAM \
-        --threads 16 \
-        --reference ${PROK_CONTIGs}/${base}_contigs_min500_Proks.fna \
-        $sample; 
-    echo .sorted.bam file for $base is now has been created
-else
-    echo "BAM file $sample already exist"
-fi
-done
+# for sample in ${BAM_FILEs}/*.out.bam;
+# do
+# base=$(basename $sample ".out.bam")
+# if [ ! -f ${RERUN_SAMSORT}/${base}.out.sorted.bam ]; then
+#     echo working on $base
+#     samtools sort \
+#         -o ${RERUN_SAMSORT}/${base}.out.sorted.bam \
+#         --output-fmt BAM \
+#         --threads 16 \
+#         --reference ${PROK_CONTIGs}/${base}_contigs_min500_Proks.fna \
+#         $sample; 
+#     echo .sorted.bam file for $base is now has been created
+# else
+#     echo "BAM file $sample already exist"
+# fi
+# done
 
-echo samtools sort completed at $(date)
+# echo samtools sort completed at $(date)
 
 conda activate MAG
 echo start regenarating coverage file at $(date)
 
 COV_Files="/gxfs_work/geomar/smomw681/DATA/MAG_Illumina/METABAT2/COVERAGE_FILEs"
-for sample in ${RERUN_SAMSORT}/*.out_sorted.bam;
+for sample in ${RERUN_SAMSORT}/*.out.sorted.bam;
 do
 if [ ! -f ${COV_Files}/${base}.depth.txt ]; then
     base=$(basename $sample ".out.sorted.bam")
