8ae761fa84f39c32672df8deeb5b710c90b534ca
Saskianne <157356003+Saskianne@users.noreply.github.com>
2025-04-10 00:56:19 +0200

    2025.04.10_md

---
+++ Backup_scripts/4_4_2_AntiSMASH_Illumina_re.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH -c 18                      # 1 core per job (i.e., if you need 8 cores, you would have to use "-c 8")
+#SBATCH --job-name=AntSMASH
+#SBATCH -t 10-00:00                # Runtime in D-HH:MM
+#SBATCH --qos=long                  # quality of service parameters
+#SBATCH -p base                  # Partition to submit to
+#SBATCH --mem=240G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=AntiSMASH_MAG_PacBio.out
+#SBATCH --error=AntiSMASH_MAG_PacBio.err
+#
+# here starts your actual program call/computation
+#
+module load gcc12-env/12.3.0
+module load miniconda3/24.11.1
+conda activate AntiSMASH
+
+cd /gxfs_work/geomar/smomw681/DATA/MAG_ALL/
+
+echo "START TIME": '' $(date)
+
+# Set up variables
+MAG_Illumina="/gxfs_work/geomar/smomw681/DATA/MAG_Illumina/METABAT2/METABAT2_PROKS_BIN"
+DBDIR="/gxfs_work/geomar/smomw681/.conda/envs/AntiSMASH/lib/python3.10/site-packages/antismash/databases"
+ANTISMASH_DIR="/gxfs_work/geomar/smomw681/DATA/MAG_Illumina/METABAT2/AntiSMASH_Illumina"
+
+for file in ${MAG_Illumina}/*.fa; do
+base=$(basename $file ".fa")
+echo working with $file
+sbatch --cpus-per-task=4 --mem=100G --wrap="antismash \
+     -t bacteria \
+     --cpus 8 \
+     --databases ${DBDIR}/ \
+     --output-dir ${ANTISMASH_DIR}/${base}/ \
+     --html-title ${base}_antiSMASH \
+     --output-basename ${base} \
+     --genefinding-tool prodigal-m \
+     $file"
+done
+
+
+echo "END TIME": '' $(date)
+#
+## Summary antiSMAH results
+## 
+# python /gxfs_work/geomar/smomw647/ComGenomicsTools/multismash/workflow/scripts/count_regions.py \
+#      /gxfs_work/geomar/smomw647/PROJECTS/SPONGEMicrobiome/COLLABORATIONs/SUNWOO/CLASS_CONTIGs/AntiSMASH_PROKS/ \
+#      AntiSMASH_PROKS_FWT_SpongeMGs_ProksContigsONLY.tsv
+##
