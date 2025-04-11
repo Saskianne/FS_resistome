eb72ea06f5470950af69181b6271dccce42e2f8e
Saskianne <157356003+Saskianne@users.noreply.github.com>
2025-04-11 17:37:56 +0200

    2025.04.11_evening

---
+++ MAG_downstream/ARG_anaylsis/5_3_3_AntiSMASH_MAG_contigs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#SBATCH -c 18                      # 1 core per job (i.e., if you need 8 cores, you would have to use "-c 8")
+#SBATCH --job-name=AntSMASH
+#SBATCH -t 10-00:00                # Runtime in D-HH:MM
+#SBATCH --qos=long                  # quality of service parameters
+#SBATCH -p base                  # Partition to submit to
+#SBATCH --mem=240G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=AntiSMASH_MAG_ALL.out
+#SBATCH --error=AntiSMASH_MAG_ALL.err
+#
+# here starts your actual program call/computation
+#
+module load gcc12-env/12.3.0
+module load miniconda3/24.11.1
+conda activate AntiSMASH
+
+cd /gxfs_work/geomar/smomw681/DATA/MAG_ALL/
+
+echo "START TIME": '' $(date)
+
+# Set up variables
+dREP_FILEs="/gxfs_work/geomar/smomw681/DATA/MAG_ALL/dREP_ALL/dereplicated_genomes"
+CTG_PacBio_DIR="/gxfs_work/geomar/smomw681/DATA/PacBio_Assembly/metaFlye"
+CTG_Illumina_Dir="/gxfs_work/geomar/smomw681/DATA/MAG_Illumina/CONTIGs_renamed"
+DBDIR="/gxfs_work/geomar/smomw681/.conda/envs/AntiSMASH/lib/python3.10/site-packages/antismash/databases"
+ANTISMASH_DIR="/gxfs_work/geomar/smomw681/DATA/MAG_ALL/AntiSMASH_ALL/AntiSMASH_CTG"
+
+for file in ${CTG_Illumina_Dir}/*.fasta; do
+base=$(basename $file "_renamed.fasta")
+sbatch --cpus-per-task=4 --mem=100G --wrap="antismash \
+     -t bacteria \
+     --cpus 8 \
+     --databases ${DBDIR}/ \
+     --output-dir ${ANTISMASH_DIR}/${base}/ \
+     --html-title ${base}_antiSMASH \
+     --output-basename ${base} \
+     --genefinding-tool prodigal-m \
+     $file"
+done
+
+for file in ${CTG_PacBio_DIR}/*/*.fasta; do
+base=$(basename $file "_assembly.fasta")
+sbatch --cpus-per-task=4 --mem=100G --wrap="antismash \
+     -t bacteria \
+     --cpus 8 \
+     --databases ${DBDIR}/ \
+     --output-dir ${ANTISMASH_DIR}/${base}/ \
+     --html-title ${base}_antiSMASH \
+     --output-basename ${base} \
+     --genefinding-tool prodigal-m \
+     $file"
+done
+
+
+
+echo "END TIME": '' $(date)
+#
+## Summary antiSMAH results
+## 
+# python /gxfs_work/geomar/smomw647/ComGenomicsTools/multismash/workflow/scripts/count_regions.py \
+#      /gxfs_work/geomar/smomw647/PROJECTS/SPONGEMicrobiome/COLLABORATIONs/SUNWOO/CLASS_CONTIGs/AntiSMASH_PROKS/ \
+#      AntiSMASH_PROKS_FWT_SpongeMGs_ProksContigsONLY.tsv
+##
