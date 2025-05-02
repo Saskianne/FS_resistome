254207df6c32a60d454a086a96b46feb30f0a49f
Saskianne <157356003+Saskianne@users.noreply.github.com>
2025-05-02 16:30:36 +0200

    2025.05.02_end

--- Jutta_strains/JA_2_PRODIGAL_extract.sh
+++ Jutta_strains/JA_2_PRODIGAL_extract.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -c 10                      # 1 core per job (i.e., if you need 8 cores, you would have to use "-c 8")
+#SBATCH -c 5                      # 1 core per job (i.e., if you need 8 cores, you would have to use "-c 8")
 #SBATCH --job-name=PROD_EXTRACT        # Job name
 #SBATCH -t 9-00:00                # Runtime in D-HH:MM
 #SBATCH --qos=long                  # quality of service parameters
 #SBATCH -p base                  # Partition to submit to                  # Partition to submit to
-#SBATCH --mem=90G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=100G                 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=Prodigal_Jutta_strain_ALL1.out
 #SBATCH --error=Prodigal_Jutta_strain_ALL1.err
 
@@ -22,23 +22,11 @@ GENOME_Jutta_eurofins="/gxfs_work/geomar/smomw681/DATA/GENOME_Jutta/Eurofins Gen
 echo "START TIME": '' $(date)
 
 # Iterate prodigal over in-house genomes
-for i in ${GENOME_Jutta}/*.fasta;
-do
-newfile="$(basename $i '.fasta')"
-if [ ! -f ${ProdDIR}/${newfile}.CDS.fna ]; then
-    echo working with $i
     pprodigal --tasks 6 --chunksize 20000 -p single -m \
-        -i $i \
+        -i ${GENOME_Jutta}/*.fasta \
         -o ${ProdDIR}/PROD_Temp.gbk \
         -d ${ProdDIR}/${newfile}.CDS.fna \
         -a ${ProdDIR}/${newfile}.ORFs.faa
-    echo done with $i
-elif [ -f ${ProdDIR}/${newfile}.CDS.fna ]; then
-    echo "File ${ProdDIR}/${newfile}.CDS.fna already exists"
-else 
-    echo "File ${ProdDIR}/${newfile}.CDS.fna does not exist and something went wrong"
-fi
-done
 
 # # Iterate prodigal over Eurofins geneomes (generated after Sept 2024)
 ## Do it later when you have access to U drive
