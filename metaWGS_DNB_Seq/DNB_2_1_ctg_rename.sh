c91c38c6ac2bc6eab56549a2c94f17e648f4fab4
Saskianne <157356003+Saskianne@users.noreply.github.com>
2026-01-06 15:20:22 +0100

    2026.01.06_mid

--- metaWGS_DNB_Seq/DNB_2_1_ctg_rename.sh
+++ metaWGS_DNB_Seq/DNB_2_1_ctg_rename.sh
@@ -8,7 +8,14 @@ CTG_DIR="/gxfs_work/geomar/smomw681/BGI_META_WGS_DATA/BGI_DATA_DAVID/ASSEMBLIES"
 
 CTG_FILES="/gxfs_work/geomar/smomw681/BGI_META_WGS_DATA/BGI_DATA_DAVID/ASSEMBLIES/*_SPADessembly/contigs.fasta"
 
+mkdir SPADES_CONTIG_FILES
 
 for ctg_files in ${CTG_FILES[@]}; do 
+    FULLDIR=$(dirname $ctg_files)
+    DIRNAME=$(basename $FULLDIR)
+    echo $DIRNAME
+    #SAMPLE_ID =  
+    #echo "Rename the $ctg_files to $FILENAME"
+    # cp ctg_files
     
 done
